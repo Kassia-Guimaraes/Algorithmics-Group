9a24589b2d5a1b71c27ddfc79b022fc7c5f0c9d7
Kassia Guimaraes <kassiavivereduarda@gmail.com>
2023-11-27 12:09:20 +0000

    Def Add Music

--- addMusic.py
+++ addMusic.py
@@ -4,27 +4,27 @@ import numpy as np
 tableMusic = pd.read_csv("./tableMusic.csv", sep=(","), index_col='id_music')
 #style  type    title   songwritter year    artist  rating_global   rating_user duration
 
-title_music = str(input("Digite o nome da música que deseja adicionar: "))
+title_music = str(input("What's the title of the song? "))
 title_music = title_music.title()
 
-style_music = input("Qual o estilo da música? ")
+style_music = input("What's the style? ")
 style_music = style_music.title()
 
-type_music = input("Qual a tipologia da música? ")		
+type_music = input("What's the typology? ")
 type_music = type_music.title()
 
-songwritter_music = input("Qual o nome do autor da música? ")	
+songwritter_music = input("What's the songwritter name? ")	
 songwritter_music = songwritter_music.title()
 
-year_music = int(input("Qual o ano de lançamento da música? "))
+year_music = int(input("what year was the song released? "))
 
-artist_music = input("Quem canta a música? ")	
+artist_music = input("Who sings the music? ")	
 artist_music = artist_music.title()
 
-rating_global_music = int(input("Qual a avaliação global da música? "))	
-rating_user_music = int(input("Qual a avaliação que você dá para a música? "))	
+rating_global_music = int(input("What's the global rating of the song? "))	
+rating_user_music = int(input("What's your rating of the song? "))	
 
-duration_music = input("Qual a duração da música? ")
+duration_music = input("how long is the song? ")
 duration_music = duration_music.split(":")
 minutes = int(duration_music[0])
 seconds = int(duration_music[1])
@@ -34,4 +34,3 @@ new_music = {'style': style_music, 'type': type_music, 'title': title_music, 'so
 
 tableMusic = tableMusic.append(new_music, ignore_index=True)
 print(tableMusic)
-print("teste")
