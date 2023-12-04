ea360eabfd3ba487c7837ecf83fc84f9f4a2c680
LCGhit <luismpcunha@protonmail.com>
2023-12-04 00:12:42 +0000

    conciseness

--- filterStyle.py
+++ filterStyle.py
@@ -2,50 +2,45 @@ import pandas as pd
 import os.path
 import addStyle
 
-def category_list(category, table):
+# create list of all elements of given category used to create menu
+def category_list(category):
     if category == "style":
         return addStyle.refresh_styles_list()
     else:
-        return (list(table.loc[:, category].drop_duplicates()))
+        return (list(filtered_songs.loc[:, category].drop_duplicates()))
 
-# standard message to be reused for other filtering messages
-def filter_menu(a):
-    print(a + " - press a number:")
-    for i in range(0, len(filter), 1):
-        print(str(i+1) + " " + str(filter[i]))
-
-# retrieves the songs with a given filter
+# retrieve the songs with a given filter
 def filterSongs(theFilter):
     picked_filter = 0
+    filtered_songs = pd.read_csv('tableMusic.csv')
     while picked_filter == 0:
         try:
-            if theFilter == "style":
-                picked_filter = filter[int(input())-1]
-                print("The " + theFilter + " picked was " + str(picked_filter) + "\n")
-                tm = pd.read_csv('tableMusic.csv')
-                tm.set_index(theFilter, inplace = True)
-                return tm.loc[picked_filter]
-            else:
-                picked_filter = filter[int(input())-1]
-                print("The " + theFilter + " picked was " + str(picked_filter) + "\n")
-                filtered_songs.set_index(theFilter, inplace = True)
-                return filtered_songs.loc[picked_filter]
+            picked_filter = filter[int(input())-1]
+            print("The " + theFilter + " picked was " + picked_filter + "\n")
+            filtered_songs.set_index(theFilter, inplace = True)
+            return filtered_songs.loc[picked_filter]
         except:
             picked_filter = 0
             print("Invalid choice.\n")
             filter_menu(theFilter)
 
-# define styles with addStyle.py function
-filter = category_list("style", pd.read_csv('tableMusic.csv'))
+# output menu for given filter
+def filter_menu(a):
+    print("\033[1m" + "FILTER " + a.upper() + "\n(press a number)" + "\033[0;0m")
+    for i in range(0, len(filter), 1):
+        print("\033[1m" + str(i+1) + "\033[0;0m " + str(filter[i]))
+    return len(filter)
 
-filter_menu("Style")
+filter_list = ["style", "artist", "songwritter", "type"]
 
-filtered_songs = filterSongs("style")
-
-filter = category_list("artist", filtered_songs)
-
-filter_menu("Artist")
-
-filtered_songs = filterSongs("artist")
-
+# def applyFilters(array):
+for i in range(0, len(filter_list), 1):
+    filter = category_list(filter_list[i])
+    if len(filter) == 1:
+        continue
+    else:
+        filter_menu(filter_list[i])
+        filtered_songs = filterSongs(filter_list[i])
 print(filtered_songs)
+
+# print(applyFilters(filter_list))
