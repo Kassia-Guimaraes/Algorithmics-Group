883a7bf50fc501da4fb63c92504344ed94507743
VG189 <151916452+VG189@users.noreply.github.com>
2023-12-04 11:51:12 +0000

    Add files via upload
    tabela de música atualizada (sem exclusão de elementos - com 120 músicas - de 0 a 119)
    tarefa: remover músicas
    tarefa: melhor avaliação por estilo

---
+++ bestRankStyle.py
@@ -0,0 +1,15 @@
+import pandas as pd
+#Retrieves all the songs of a certain style
+def getSongsOfStyle(tableMusic, style):
+    #returns a subset of the dataframe "tableMusic"
+    #with the songs of style corresponding to the argument "style"
+    return tableMusic[tableMusic["style"] == style]    
+
+# the function ranking that create a ranking of the style selected, 
+# if it's greater than 3, and sort in descending order.
+def rankingStyle(songs):
+    bestSongs = songs[songs["rating_global"] > 3] 
+    return bestSongs.sort_values("rating_global", ascending=False)
+           
+
+ 
---
+++ removeSong.py
@@ -0,0 +1,32 @@
+from auxiliarFunctions import getPlaylist
+
+def removeSongDataBase(tableMusic, playlists, id):
+    
+    if id in tableMusic["id_music"].values:
+       tableMusic.drop(tableMusic[tableMusic["id_music"] == id].index,inplace=True)
+       playlists.drop(playlists[playlists["id_music"] == id].index, inplace = True)
+       tableMusic.to_csv("tableMusic.csv",index=False)
+       playlists.to_csv("playlist.csv",index=False)     
+    else : 
+        print("This song does not exist in our system. Please chose a valid option:")
+        
+def removeSongPlaylist(playlists, playListName, id):
+    #gets chosen playlist
+    playlist = getPlaylist(playlists,playListName)
+    
+    #checks if chosen id is in playlist
+    if id in playlist["id_music"].values:
+        #get boolean array of playlist matches
+        playListMatch = playlists["id_playlist"] == playListName
+        #get boolean array of music id matches
+        idMatch = playlists["id_music"] == id
+        #get indexes of songs to remove from playlist
+        removeIndex = playlists[playListMatch & idMatch].index
+        #removes from playLists dataframe the specified song of the specified playlist
+        playlists.drop(removeIndex, inplace = True)
+        playlists.to_csv("playlist.csv",index = False)     
+    else : 
+        print("The chosen playlist doesnt exist or the song is not in the playlist. Please chose a valid option:")
+
+    
+    
