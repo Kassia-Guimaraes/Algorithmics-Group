2edfe24f2e7dc9fa9c5f95129d696eb7c8497c8a
LCGhit <luismpcunha@protonmail.com>
2023-12-05 21:07:49 +0000

    fix: both string and int in table can be used

--- filterStyle.py
+++ filterStyle.py
@@ -5,15 +5,16 @@ import os.path
 
 filtered_songs = pd.read_csv('data/tableMusic.csv')
 
-filter_list = ["type", "songwriter", "artist", "style"]
+filter_list = ["year", "artist", "style", "type"]
 
 # create list of all elements of given category used to create menu
 def category_list(category):
     try:
         print(filtered_songs)
         # print(filtered_songs.loc[:, category])
-        return (list(filtered_songs.loc[:, category].astype(str).drop_duplicates()))
+        return (list(filtered_songs.loc[:, category].drop_duplicates()))
     except:
+        print("erro")
         return
 
 # retrieve the songs with a given filter
@@ -22,39 +23,26 @@ def filterSongs(theFilter):
     filtered_songs = pd.read_csv('data/tableMusic.csv')
     while picked_filter == 0:
         try:
-            picked_filter = filter[int(input())-1]
-            print("The " + theFilter + " picked was " + picked_filter + "\n")
             filtered_songs.set_index(theFilter, inplace = True)
-            return filtered_songs.loc[picked_filter]
-        except Exception as e:
+            return filtered_songs.loc[[filter[int(input("=> "))-1]]]
+        except:
             picked_filter = 0
             print("Invalid choice.\n")
-            print(e)
             filter_menu(theFilter)
 
 # output menu for given filter
 def filter_menu(a):
     print("\033[1m" + "FILTER " + a.upper() + "\n(press a number)" + "\033[0;0m")
     for i in range(0, len(filter), 1):
-        print("\033[1m" + str(i+1) + "\033[0;0m " + str(filter[i]))
+        print("\033[1m", str(i+1), "\033[0;0m ", str(filter[i]))
     return len(filter)
 
-# def applyFilters(array):
 for i in range(0, len(filter_list), 1):
     try:
         filter = category_list(filter_list[i])
-        print(filter)
-
         filter_menu(filter_list[i])
         filtered_songs = filterSongs(filter_list[i])
-
-        # if len(filter) == 1:
-        #     print("continues")
-        #     continue
-        # else:
-        #     filter_menu(filter_list[i])
-        #     filtered_songs = filterSongs(filter_list[i])
     except Exception as e:
-        print("")
+        print(e)
 
 print(filtered_songs)
