7241db54db3707e17227fdca27029b463da4b965
LCGhit <luismpcunha@protonmail.com>
2023-12-07 09:59:30 +0000

    filters applied with single function

--- filterStyle.py
+++ filterStyle.py
@@ -1,46 +1,46 @@
 import pandas as pd
 import os.path
 
-# pd.set_option('display.max_columns', 1000)
-
-filtered_songs = pd.read_csv('data/tableMusic.csv')
+pd.set_option('display.max_columns', 1000)
 
 filter_list = ["year", "artist", "style", "type"]
 
 # create list of all elements of given category used to create menu
-def category_list(category):
+def category_list(category, table):
     try:
-        # print(filtered_songs.loc[:, category])
-        return (list(filtered_songs.loc[:, category].drop_duplicates()))
+        return (list(table.loc[:, category].drop_duplicates()))
     except:
         print("erro")
         return
 
 # retrieve the songs with a given filter
-def filterSongs(theFilter):
+def filterSongs(theFilter, table, filterArray):
     picked_filter = 0
     while picked_filter == 0:
         try:
-            filtered_songs.set_index(theFilter, inplace = True)
-            return filtered_songs.loc[[filter[int(input(" => "))-1]]]
+            table.set_index(theFilter, inplace = True)
+            return table.loc[[filterArray[int(input(" => "))-1]]]
         except:
             picked_filter = 0
             print("Invalid choice.\n")
-            filter_menu(theFilter)
+            filter_menu(theFilter, filterArray)
 
 # output menu for given filter
-def filter_menu(a):
-    print("\033[1m" + "FILTER " + a.upper() + "\n(press a number)" + "\033[0;0m")
-    for i in range(0, len(filter), 1):
-        print("\033[1m", str(i+1), "\033[0;0m ", str(filter[i]))
-    return len(filter)
+def filter_menu(theArray, theFilter):
+    print("\n" + "\033[1m" + "FILTER " + theArray.upper() + "\n(press a number)" + "\033[0;0m")
+    for i in range(0, len(theFilter), 1):
+        print("\033[1m", str(i+1), "\033[0;0m ", str(theFilter[i]))
+    return len(theFilter)
 
-for i in range(0, len(filter_list), 1):
-    try:
-        filter = category_list(filter_list[i])
-        filter_menu(filter_list[i])
-        filtered_songs = filterSongs(filter_list[i])
-    except Exception as e:
-        print(e)
+def applyFilters(filtersList, table):
+    filtered_songs = pd.read_csv(table)
+    for i in range(0, len(filtersList), 1):
+        try:
+            filter = category_list(filtersList[i], filtered_songs)
+            filter_menu(filtersList[i], filter)
+            filtered_songs = filterSongs(filtersList[i], filtered_songs, filter)
+        except Exception as e:
+            print(e)
+    print(filtered_songs)
 
-print(filtered_songs)
+applyFilters(['artist', 'year', 'songwriter', 'type'], 'data/tableMusic.csv')
