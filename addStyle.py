c4cc334cf4fec9b37e0b75a75340ca4d83ca562e
LCGhit <luismpcunha@protonmail.com>
2023-12-19 13:05:32 +0000

    fix: everything inside functions; path to file corrected

--- addStyle.py
+++ addStyle.py
@@ -4,16 +4,15 @@ from pandas.core.internals.blocks import new_block
 
 def refresh_styles_list():
     # read populate styles with the "styles.csv" from previous execution, if it exists
-    if(os.path.isfile('styles.csv')):
+    if(os.path.isfile('data/styles.csv')):
         return (list(pd.read_csv('data/styles.csv').loc[:, "0"]))
     # otherwise extract styles from tableMusic.csv
     else:
         tm = pd.read_csv('data/tableMusic.csv')
         return (list(tm.loc[:, "style"].drop_duplicates()))
 
-styles = refresh_styles_list()
-
 def addStyle():
+    styles = refresh_styles_list()
     print("Current styles: " + ', '.join(styles))
     new_Style = (input("What new style would you like to add?\nPress 0 to cancel\n=> "))
     if new_Style == "0":
@@ -23,9 +22,8 @@ def addStyle():
         styles.append(new_Style)
         styles_Df = pd.DataFrame(styles)
         # create/update styles.csv with the new style
-        styles_Df.to_csv('styles.csv', index=False, na_rep="NAN!")
+        styles_Df.to_csv('data/styles.csv', index=False, na_rep="NAN!")
         print(f"The style {new_Style} was added successfully!")
     return new_Style
 
-if __name__ == "__main__":
-    addStyle()
+addStyle()
