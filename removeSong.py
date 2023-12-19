597ebddf548edc3bbaac00f66539e59b4217ae29
LCGhit <luismpcunha@protonmail.com>
2023-12-19 20:52:25 +0000

    updated removeSongMain.py

--- removeSong.py
+++ removeSong.py
@@ -2,19 +2,19 @@ from auxiliarFunctions import getPlaylist
 import errorCodes as ec
 
 def removeSongDataBase(tableMusic, playlists, id):
-    
+
     if id in tableMusic["id_music"].values:
        tableMusic.drop(tableMusic[tableMusic["id_music"] == id].index,inplace=True)
        playlists.drop(playlists[playlists["id_music"] == id].index, inplace = True)
-       tableMusic.to_csv("data\\tableMusic.csv", index=False)
-       playlists.to_csv("data\\playlist.csv", index=False)     
-    else : 
+       tableMusic.to_csv("data/tableMusic.csv", index=False)
+       playlists.to_csv("data/playlist.csv", index=False)
+    else :
         print("This song does not exist in our system. Please chose a valid option:")
-        
+
 def removeSongPlaylist(playlists, playListName, id):
     #gets chosen playlist
     playlist = getPlaylist(playlists,playListName)
-    
+
     #checks if chosen id is in playlist
     if id in playlist["id_music"].values:
         #get boolean array of playlist matches
@@ -25,14 +25,11 @@ def removeSongPlaylist(playlists, playListName, id):
         removeIndex = playlists[playListMatch & idMatch].index
         #removes from playLists dataframe the specified song of the specified playlist
         playlists.drop(removeIndex, inplace = True)
-        
+
         try:
-            playlists.to_csv("data\\playlist.csv", index = False)  
-        
+            playlists.to_csv("data/playlist.csv", index = False)
+
         except:
-            return ec.file_open      
-    else : 
+            return ec.file_open
+    else :
         print("The chosen playlist doesnt exist or the song is not in the playlist. Please chose a valid option:")
-
-
-    
