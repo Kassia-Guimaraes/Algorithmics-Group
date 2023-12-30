20ccc8ef28d44d05da2be65af9c3b84d48954b50
Vanessa Gomes <vanessa.gomes189@gmail.com>
2023-12-30 11:06:00 +0000

    removeSong.py average song rating update added

--- errorCodes.py
+++ errorCodes.py
@@ -6,7 +6,8 @@ messages = ["INFO: Successfull execution",
                   "ERROR: Sintax error",
                   "ERROR: The specified song is already present in playlist",
                   "ERROR: The specified song was not found in the database",
-                  "ERROR: The program can not be executed when the file is open. Please close the file and try again"]
+                  "ERROR: The program can not be executed when the file is open. Please close the file and try again",
+                  "ERROR: The specified song was not found in the playlist"]
 
 successfull_execution = 0
 playlist_not_found = 1
@@ -15,3 +16,4 @@ sintax = 3
 playlist_duplicate = 4 
 song_not_found = 5
 file_open = 6
+song_not_found_playlist = 7
--- removeSong.py
+++ removeSong.py
@@ -1,42 +1,99 @@
+import pandas as pd
 from auxiliarFunctions import getPlaylist
 import errorCodes as ec
+import auxiliarFunctions as af
+
+tableMusic = pd.read_csv('data\\tableMusic.csv')
 
 # function responsible for remove a song for the database and for the playlist in the same time.
-def removeSongDataBase(tableMusic, playlists, id):
+def removeSongDataBase(tableMusic, playlists):
     #the condition sees if the index is in the dataframe tableMusic
-    if id in tableMusic["id_music"].values:
-       # .drop is the pandas function to remove anything; and get boolean array of music id matches
-       tableMusic.drop(tableMusic[tableMusic["id_music"] == id].index,inplace=True)
-       playlists.drop(playlists[playlists["id_music"] == id].index, inplace = True)
-       #tableMusic and playlists are the dataframes where the music is going to be removed.
-       tableMusic.to_csv("data\\tableMusic.csv", index=False)
-       playlists.to_csv("data\\playlist.csv", index=False)     
-    else : 
-        print("This song does not exist in our system. Please chose a valid option:")
-        
+    #Testing function that removes a song from the entire database (tableMusic and playlists)
+    print("Choose one song to remove from the data base")
     
-def removeSongPlaylist(playlists, playListName, id):
+    songId = input("Enter song id: ")
+    try:
+        songId = int(songId)
+        
+    except:
+        return ec.sintax
+    
+    if not((songId == tableMusic["id_music"]).any()):
+        return ec.song_not_found   
+   
+    # .drop is the pandas function to remove anything; and get boolean array of music id matches
+    tableMusic.drop(tableMusic[tableMusic["id_music"] == songId].index, inplace=True)
+    playlists.drop(playlists[playlists["id_music"] == songId].index, inplace = True)
+        
+         
+    try: 
+        tableMusic.to_csv("data\\tableMusic.csv", index= False) 
+        print(tableMusic)
+        
+    except:
+        return ec.file_open
+    
+    print(tableMusic)
+    return ec.successfull_execution
+
+#function that remove a song from a playlist
+def removeSongPlaylist(songDataBase, playlists):
+    
     #gets chosen playlist
-    playlist = getPlaylist(playlists,playListName)
+    playListName = input("Enter a playlist name: ")
+    playlist = getPlaylist(playlists, playListName)
     
-    #checks if chosen id is in playlist
-    if id in playlist["id_music"].values:
-        #get boolean array of playlist matches
-        playListMatch = playlists["id_playlist"] == playListName
-        #get boolean array of music id matches
-        idMatch = playlists["id_music"] == id
-        #get indexes of songs to remove from playlist
-        removeIndex = playlists[playListMatch & idMatch].index
-        #removes from playLists dataframe the specified song of the specified playlist
-        playlists.drop(removeIndex, inplace = True)
+    numSongs = len(playlist)
+    
+    #if it's empyte the condition will return an error
+    if numSongs <= 0 :
+        return ec.playlist_not_found
+
+    #user input of the playlist  
+    print("You chose the playlist: ", playListName)    
+    
+    # music that the user wishes to remove in the playlist 
+    songId = input("Enter index song to be removed from the playlist: ")
+    
+    #the input of the music have to be an integer as it is being taken from the column 
+    # 'id_music'. if it´s not it will return an error.
+    try:
+        songId = int(songId)
         
-        try:
-            playlists.to_csv("data\\playlist.csv", index = False)  
-        #error message from file errorCode
-        except:
-            return ec.file_open      
-    else : 
-        print("The chosen playlist doesnt exist or the song is not in the playlist. Please chose a valid option:")
-
-
+    except:
+        return ec.sintax
+    
+ 
+    if not ((songId == playlist["id_music"]).any()):
+        return ec.song_not_found_playlist
+     
+    songRating = songDataBase['rating_global'][songId == songDataBase["id_music"]].item()
+    #calculate the new average song rating and put with one decimal place.
+    newAverageSongRating = af.subtractFromAverage(playlist["average_rating_musics"][0], numSongs, songRating)
+    newAverageSongRating = round(newAverageSongRating, 1)
+    
+    playlists["average_rating_musics"][(playlists["id_playlist"] == playListName)] = newAverageSongRating
+    
+    #get boolean array of playlist matches
+    playListMatch = playlists["id_playlist"] == playListName
+    #get boolean array of music id matches
+    idMatch = playlists["id_music"] == songId
+    #get indexes of songs to remove from playlist
+   
+    removeIndex = playlists[playListMatch & idMatch].index
+ 
+    #removes from playLists dataframe the specified song of the specified playlist
+    playlists.drop(removeIndex, inplace = True)
+       
+    
+    try:
+        playlists.to_csv("data\\playlist.csv", index = False)  
+    
+    except:
+        # error message from file errorCode
+        return ec.file_open 
+       
+    print(getPlaylist(playlists, playListName).to_markdown(index=False))
+    
+    return ec.successfull_execution   
     
